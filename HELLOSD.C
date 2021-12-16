c9a28843c9fb981be3c134a5166b5e18e26c039f
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-12-16 13:09:55 +0530

    Add files via upload

---
+++ HELLOSD.C
@@ -0,0 +1,22 @@
+//Pre-Processor Directive
+//#: Pre-Processor
+//stdio.h: directive/directory/folder
+#include<stdio.h>
+//stdio.h: header file, stdio: standard input / output
+/*
+comment: It is used to describe your code or hide your code.
+Comment is also not a part of code and ignored by the compiler.
+*/
+//: single line comment
+/*
+	:Multiple Line Comment
+*/
+//void : null or empty
+//(): function
+void main()   // main(): entry point of every c program
+{
+	clrscr(); // clear screen
+	printf("Hello World"); // print the output on the
+//	output/terminal/console screen
+	getch(); // get character
+}
