ae85e20270eb35d1b2a02506b73f67be9da5f066
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-12-19 17:06:09 +0530

    Add files via upload

---
+++ SUMS.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+	int a, b;
+	clrscr();
+
+	printf("Enter value for a and b:");
+	scanf("%d %d", &a, &b);
+	printf("Addition is: %d", (a + b));
+
+       getch();
+}
